b3698207b83f13706798435b5647c5262fd2f129
unknown <saiswaroop17254@gmail.com>
2024-09-03 12:51:57 +0530

    added the classes to test class

--- src/test/java/com/project/ets/EducationTrackingSystemApplicationTests.java
+++ src/test/java/com/project/ets/EducationTrackingSystemApplicationTests.java
@@ -3,7 +3,7 @@ package com.project.ets;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = EducationTrackingSystemApplication.class)
 class EducationTrackingSystemApplicationTests {
 
 	@Test
