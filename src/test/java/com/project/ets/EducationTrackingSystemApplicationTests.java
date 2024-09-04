09219970836f89fbce210734daca3b59749d3d75
unknown <saiswaroop17254@gmail.com>
2024-09-04 11:47:05 +0530

    removed test method

--- src/test/java/com/project/ets/EducationTrackingSystemApplicationTests.java
+++ src/test/java/com/project/ets/EducationTrackingSystemApplicationTests.java
@@ -3,12 +3,7 @@ package com.project.ets;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest(classes = EducationTrackingSystemApplication.class)
-class EducationTrackingSystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-		throw new UnsupportedOperationException();
-	}
+class EducationTrackingSystemApplicationTests {
 
 }
