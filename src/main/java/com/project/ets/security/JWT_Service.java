bf0bcbbcc93f39429d1460a7ca2053cccfc3fe54
unknown <saiswaroop17254@gmail.com>
2024-09-16 12:14:31 +0530

    add the jwt dependencies and created JWT Service which will have payload and signature

--- src/main/java/com/project/ets/security/JWT_Service.java
+++ src/main/java/com/project/ets/security/JWT_Service.java
@@ -1,7 +1,15 @@
 package com.project.ets.security;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 
 @Service
 public class JWT_Service {
@@ -9,5 +17,19 @@ public class JWT_Service {
     @Value("${myapp.jwt.secret}" )
     private String secret;
     @Value("${myapp.jwt.access_expiry}")
-    private int access_expiry;
+    private long access_expiry;
+
+
+    public String createJwt(String userId,String email,String role){
+       return Jwts.builder()
+                .setClaims(Map.of("userId",userId,"email",email,"role",role))
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis()+access_expiry*60*1000))
+                .signWith(getSigninKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    private Key getSigninKey(){
+       return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secret));
+    }
 }
